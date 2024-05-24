5eaf7729951f30ec917a5de35fc6fecb82a8e815
Mabende <belindamabende@gmail.com>
2024-05-25 01:12:04 +0200

    How long I am?

---
+++ count_me.js
@@ -0,0 +1,4 @@
+let yourMessage = prompt("Message: ");
+let myLimit = 280;
+let yourRemainingCharacters = 280 - yourMessage.length;
+alert("You have entered " + yourMessage.length + " characters, " + "you have " + yourRemainingCharacters + " left.")
